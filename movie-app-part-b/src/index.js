29346be968bcebc58961bdedcd10feeb0a29c816
TheTaiser <tnihore.536@gmail.com>
2021-05-12 13:21:05 +0530

    Checkpoint 2

--- movie-app-part-b/src/index.js
+++ movie-app-part-b/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'typeface-roboto';
+// import 'typeface-roboto';
 import registerServiceWorker from './registerServiceWorker';
-import Controller from './screens/Controller.js';
+import Controller from './screens/Controller';
 
 ReactDOM.render(<Controller />, document.getElementById('root'));
 registerServiceWorker();
